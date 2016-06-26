00c1abb6090825d72788b902da31d9b2bbbb434d
Nathan Specht <nathanspecht@gmail.com>
2016-06-26 14:55:41 -0500

    Use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151012212038) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "benches", force: :cascade do |t|
     t.text    "description", null: false
     t.float   "lat",         null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Bench.create(description: 'Four Barrel Coffee Bench', lat: '37.766956', lng: '-122.421932', seats: 3, img_url: "http://www.redwoodbridges.com/images-gardenbenches/garden-bench5.jpg")
-Bench.create(description: 'Roxie Thater Bench', lat: '37.764500', lng: '-122.422492', seats: 4, img_url: "http://img.archiexpo.com/images_ae/photo-g/public-bench-contemporary-metal-backrest-54663-2051507.jpg")
+Bench.create(description: 'Roxie Theater Bench', lat: '37.764500', lng: '-122.422492', seats: 4, img_url: "http://img.archiexpo.com/images_ae/photo-g/public-bench-contemporary-metal-backrest-54663-2051507.jpg")
 Bench.create(description: 'Eureka Valley Rec Center Bench', lat: '37.759500, ', seats: 1, lng: '122.436656', img_url: "")
 Bench.create(description: 'Zazie Bench', lat: '37.765260, ', lng: '-122.449988', seats: 4, img_url: "http://img.archiexpo.com/images_ae/photo-g/public-bench-contemporary-wood-metal-54663-4972691.jpg")
